ffce973777fac4e3f75edf64d370b11a9b79fb1e
Örs Kovács <46872866+kors12@users.noreply.github.com>
2023-10-07 17:34:09 +0200

    Calculate the distance between two elements using a given function

--- DynamicTimeWarping/Program.cs
+++ DynamicTimeWarping/Program.cs
@@ -16,41 +16,46 @@ Console.WriteLine();
 var source = sourceString.Split(separator).Select(s => double.Parse(s)).ToArray();
 var target = targetString.Split(separator).Select(s => double.Parse(s)).ToArray();
 
-var dtwResult = Dtw(source, target);
+var dtwResult = DtwMatrix(source, target, (s, t) => Math.Abs(s - t));
 
-for (var i = 1; i < source.Length + 1; i++)
+WriteMatrixToConsole(source.Length + 1, target.Length + 1, dtwResult);
+
+static void WriteMatrixToConsole<T>(int n, int m, T[,] matrix)
 {
-    for (var j = 1; j < target.Length + 1; j++)
+    for (var i = 0; i < n; i++)
     {
-        Console.Write(dtwResult[i, j]);
-        Console.Write(' ');
+        for (var j = 0; j < m; j++)
+        {
+            Console.Write(matrix[i, j]);
+            Console.Write(' ');
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
 }
 
-static T[,] Dtw<T>(T[] source, T[] target)
-    where T : INumber<T>, IMinMaxValue<T>
+static TResult[,] DtwMatrix<T, TResult>(T[] source, T[] target, Func<T, T, TResult> distance)
+    where TResult : INumber<TResult>, IMinMaxValue<TResult>
 {
     var n = source.Length + 1;
     var m = target.Length + 1;
-    var dtw = new T[n, m];
+    var dtw = new TResult[n, m];
 
     for (var i = 0; i < n; i++)
     {
         for (var j = 0; j < m; j++)
         {
-            dtw[i, j] = T.MaxValue;
+            dtw[i, j] = TResult.MaxValue;
         }
     }
 
-    dtw[0, 0] = T.Zero;
+    dtw[0, 0] = TResult.Zero;
 
     for (var i = 1; i < n; i++)
     {
         for (var j = 1; j < m; j++)
         {
-            var cost = T.Abs(source[i - 1] - target[j - 1]);
-            var minPrev = new T[] {
+            var cost = distance(source[i - 1], target[j - 1]);
+            var minPrev = new TResult[] {
                 dtw[i - 1, j],
                 dtw[i, j - 1],
                 dtw[i - 1, j - 1]
