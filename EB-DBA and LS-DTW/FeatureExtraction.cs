260c6b005dfbc60e28421ac4aa4d91d8fb3835aa
Örs Kovács <46872866+kors12@users.noreply.github.com>
2023-11-09 10:31:49 +0100

    Normalization

--- EB-DBA and LS-DTW/FeatureExtraction.cs
+++ EB-DBA and LS-DTW/FeatureExtraction.cs
@@ -28,62 +28,80 @@ class FeatureExtraction : PipelineBase, ITransformation
         var dF = new List<double>();
 
         dF.Add(f[1] - f[0]);
-        if (dF[^1] == 0)
-            dF[^1] = 4.94065645841246544E-324;
         dF.Add(f[2] - f[1]);
-        if (dF[^1] == 0)
-            dF[^1] = 4.94065645841246544E-324;
 
         for (int i = 2; i < f.Count - 2; i++)
         {
             var df = 0.1 * (f[i + 1] - f[i - 1] + 2 * (f[i + 2] - f[i - 2]));
             dF.Add(df);
-            if (dF[^1] == 0)
-                dF[^1] = 4.94065645841246544E-324;
         }
 
         dF.Add(f[^2] - f[^3]);
-        if (dF[^1] == 0)
-            dF[^1] = 4.94065645841246544E-324;
         dF.Add(f[^1] - f[^2]);
-        if (dF[^1] == 0)
-            dF[^1] = 4.94065645841246544E-324;
 
         return dF;
     }
 
     public void Transform(Signature signature)
     {
-        var normX = signature.GetFeature(InputNormalizedX);
-        var normY = signature.GetFeature(InputNormalizedY);
+        var normX = signature.GetFeature(InputNormalizedX).Normalize();
+        var normY = signature.GetFeature(InputNormalizedY).Normalize();
 
-        var dX = Derivative(normX);
-        var dY = Derivative(normY);
+        var dX = Derivative(normX).Normalize();
+        var dY = Derivative(normY).Normalize();
 
         var th = dX.Zip(dY, (dx, dy) => (dx, dy))
             .Select(_ => Math.Atan(_.dy / _.dx))
-            .ToList();
+            .ToList()
+            .Normalize();
         signature.SetFeature(OutputPathTangentAngle, th);
         Progress = 25;
 
         var v = dX.Zip(dY, (dx, dy) => (dx, dy))
             .Select(_ => Math.Sqrt(_.dx *_.dx + _.dy * _.dy))
-            .ToList();
+            .ToList()
+            .Normalize();
         signature.SetFeature(OutputPathVelocityMagnitude, v);
         Progress = 50;
 
-        var dTh = Derivative(th);
+        var dTh = Derivative(th).Normalize();
         var rho = dTh.Zip(v, (dth, v) => (dth, v))
             .Select(_ => Math.Log(_.v / _.dth))
-            .ToList();
+            .ToList()
+            .Normalize();
         signature.SetFeature(OutputLogCurvatureRadius, rho);
         Progress = 75;
 
-        var dV = Derivative(v);
+        var dV = Derivative(v).Normalize();
         var alpha = dV.Zip(v, (dv, v) => (v, dv)).Zip(dTh, (_, dth) => (_.v, _.dv, dth))
             .Select(_ => Math.Sqrt(_.dv * _.dv + _.v * _.v * _.dth * _.dth))
-            .ToList();
+            .ToList()
+            .Normalize();
         signature.SetFeature(OutputTotalAccelerationMagnitude, alpha);
         Progress = 100;
+    }
+}
+
+internal static class TimeSeriesExtension
+{
+    public static List<double> Normalize(this List<double> ts)
+    {
+        // Calculate the mean
+        var mean = ts.Average();
+
+        // Subtract the mean from each data point (zero mean)
+        var zeroMean = ts.Select(x => x - mean).ToList();
+
+        // Calculate the standard deviation
+        var stdDev = Math.Sqrt(zeroMean.Average(z => z * z));
+
+        // Divide each data point by the standard deviation (unit variance)
+        var normalized = zeroMean.Select(z => z / stdDev).ToList();
+
+        var min = normalized.Min();
+
+        var translated = normalized.Select(n => n - min).ToList();
+
+        return translated.Select(t => t == 0 ? 4.94065645841246544E-324 : t).ToList();
     }
 }
