04235ede37db7a2b5d759d32903215b69cb436d0
Örs Kovács <46872866+kors12@users.noreply.github.com>
2023-11-24 00:06:34 +0100

    Run benchmark

--- EB-DBA and LS-DTW/Program.cs
+++ EB-DBA and LS-DTW/Program.cs
@@ -3,53 +3,55 @@ using SigStat.Common;
 using SigStat.Common.Pipeline;
 using SigStat.Common.Transforms;
 using EbDbaAndLsDtw;
+using SigStat.Common.Framework.Samplers;
+using SigStat.Common.Model;
 
 var path = args[0];
-var loader = new Svc2004Loader(path, true);
-var signers = new List<Signer>(loader.EnumerateSigners());
-var signaturesOfUser1 = signers[0].Signatures;
 
-foreach (var signature in signaturesOfUser1)
+var benchmark = new VerifierBenchmark()
 {
-    new SequentialTransformPipeline
+    Loader = new Svc2004Loader(path, true),
+    Verifier = new Verifier()
     {
-        new CentroidExtraction
+        Classifier = new EbDbaAndLsDtwClassifier(),
+        Pipeline = new SequentialTransformPipeline
         {
-            Inputs = new() { Features.X, Features.Y, },
-            OutputCentroid = UtilityFeatures.Centroid,
+            new CentroidExtraction
+            {
+                Inputs = new() { Features.X, Features.Y, },
+                OutputCentroid = UtilityFeatures.Centroid,
+            },
+            new Minimum { Input = Features.X, OutputMin = UtilityFeatures.MinX },
+            new Minimum { Input = Features.Y, OutputMin = UtilityFeatures.MinY },
+            new Maximum { Input = Features.X, OutputMax = UtilityFeatures.MaxX },
+            new Maximum { Input = Features.Y, OutputMax = UtilityFeatures.MaxY },
+            new Preprocessing {
+                InputX = Features.X,
+                InputY = Features.X,
+                Centroid = UtilityFeatures.Centroid,
+                MaxX = UtilityFeatures.MaxX,
+                MinX = UtilityFeatures.MinX,
+                MaxY = UtilityFeatures.MaxY,
+                MinY = UtilityFeatures.MinY,
+                OutputNormalizedX = AdditionalFeatures.NormalizedX,
+                OutputNormalizedY = AdditionalFeatures.NormalizedY,
+            },
+            new FeatureExtraction
+            {
+                InputNormalizedX = AdditionalFeatures.NormalizedX,
+                InputNormalizedY = AdditionalFeatures.NormalizedY,
+                OutputPathTangentAngle = AdditionalFeatures.PathTangentAngle,
+                OutputPathVelocityMagnitude = AdditionalFeatures.PathVelocityMagnitude,
+                OutputLogCurvatureRadius = AdditionalFeatures.LogCurvatureRadius,
+                OutputTotalAccelerationMagnitude = AdditionalFeatures.TotalAccelerationMagnitude,
+            }
         },
-        new Minimum { Input = Features.X, OutputMin = UtilityFeatures.MinX },
-        new Minimum { Input = Features.Y, OutputMin = UtilityFeatures.MinY },
-        new Maximum { Input = Features.X, OutputMax = UtilityFeatures.MaxX },
-        new Maximum { Input = Features.Y, OutputMax = UtilityFeatures.MaxY },
-        new Preprocessing {
-            InputX = Features.X,
-            InputY = Features.X,
-            Centroid = UtilityFeatures.Centroid,
-            MaxX = UtilityFeatures.MaxX,
-            MinX = UtilityFeatures.MinX,
-            MaxY = UtilityFeatures.MaxY,
-            MinY = UtilityFeatures.MinY,
-            OutputNormalizedX = AdditionalFeatures.NormalizedX,
-            OutputNormalizedY = AdditionalFeatures.NormalizedY,
-        },
-        new FeatureExtraction
-        {
-            InputNormalizedX = AdditionalFeatures.NormalizedX,
-            InputNormalizedY = AdditionalFeatures.NormalizedY,
-            OutputPathTangentAngle = AdditionalFeatures.PathTangentAngle,
-            OutputPathVelocityMagnitude = AdditionalFeatures.PathVelocityMagnitude,
-            OutputLogCurvatureRadius = AdditionalFeatures.LogCurvatureRadius,
-            OutputTotalAccelerationMagnitude = AdditionalFeatures.TotalAccelerationMagnitude,
-        }
-    }.Transform(signature);
-}
+    },
+    Sampler = new FirstNSampler()
+};
 
-var classifier = new EbDbaAndLsDtwClassifier();
+BenchmarkResults result = benchmark.Execute(true);
 
-var trainedModel = classifier.Train(signaturesOfUser1.Where(s => s.Origin == Origin.Genuine).ToList());
-
-foreach (var s in signaturesOfUser1)
-    classifier.Test(trainedModel, s);
-
-Console.WriteLine();
+Console.WriteLine($"AER: {result.FinalResult.Aer}");
+Console.WriteLine($"FAR: {result.FinalResult.Far}");
+Console.WriteLine($"FRR: {result.FinalResult.Frr}");
